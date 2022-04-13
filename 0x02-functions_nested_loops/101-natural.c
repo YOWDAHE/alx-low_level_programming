43b18ebd110ad525412af731a2f82ab5b444c072
YOWDA <ketemayodahe@gmail.com>
2022-04-13 22:18:52 +0300

    file one

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    while(1==1)
+    {
+        printf("hi");
+    }
+
+    return 0;
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i)
+{
+    if (i>0){printf("positive");}
+    else{printf("negative");}
+}
+
+#endif /* MAIN_H */
