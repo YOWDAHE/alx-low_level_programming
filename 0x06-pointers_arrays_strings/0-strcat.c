724ea3c893e3b0fc47f7e537d15be814357c23d8
YOWDA <ketemayodahe@gmail.com>
2022-04-20 21:40:31 +0300

    trying this

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+//#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
